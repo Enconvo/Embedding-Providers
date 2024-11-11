a166c4c4d17737f404fce2612308209ca36939e6
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2024-11-11 17:05:02 +0800

    空字符串的情况处理

--- src/embeddings_provider.ts
+++ src/embeddings_provider.ts
@@ -12,12 +12,19 @@ export abstract class EmbeddingsProvider {
     protected abstract _embed(input: string[], options?: EmbeddingsOptions): Promise<number[][]>;
 
     async embed(input: string[], options?: EmbeddingsOptions): Promise<number[][]> {
+        // If all strings are empty, return an array of empty arrays
+        if (input.every(str => str.trim() === '')) {
+            const dimension = this.options.modelName.dimension;
+            return Array(input.length).fill(Array(dimension).fill(0));
+        }
+
+
         let retries = 3;
         while (retries > 0) {
             try {
                 return await this._embed(input, options);
             } catch (error) {
-                console.error(`Error on attempt ${4-retries}/3:`, error);
+                console.error(`Error on attempt ${4 - retries}/3:`, error);
                 retries--;
                 if (retries === 0) {
                     const dimension = this.options.modelName.dimension;
